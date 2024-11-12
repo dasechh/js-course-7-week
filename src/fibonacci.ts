a9e9ef82ecd3057145878690b75c3535ceba6e2f
ondryusha <mac@MacBook-Pro--ondryusha.local>
2024-11-12 17:28:25 +0300

    deleted classic realization

--- src/fibonacci.ts
+++ src/fibonacci.ts
@@ -1,11 +1,5 @@
 import _ from 'lodash';
 
-// Классическая рекурсивная реализация получения числа Фибоначчи
-export function fibonacciRecursive(n: number): number {
-  if (n <= 1) return n;
-  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
-}
-
 // Оптимизированная реализация получения числа Фибоначчи с использованием lodash.memoize
 export function fibonacciMemoized(n: number): number {
   const getFibonacci = _.memoize((num: number): number => {
